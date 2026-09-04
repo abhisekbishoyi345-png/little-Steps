5d1c148877520ae74b63d21be45a92bc8c067c64
Abhisek Bishoyi <abhisekbishoyi345@gmail.com>
2026-09-04 06:42:34 +0530

    Fix API import

--- client/src/pages/AdminDashboard/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard/AdminDashboard.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import API from "src/services/api";
+import API from "../services/api";
 
 function AdminDashboard() {
   // =================================
